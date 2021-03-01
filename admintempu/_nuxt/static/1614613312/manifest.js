d1ba54e16824c5ef1e986f2457e70e33d4bcda82
devilmarte <79588260+devilmarte@users.noreply.github.com>
2021-03-02 00:48:06 +0900

    Delete admintempu directory

--- admintempu/_nuxt/static/1614613312/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fboard","\u002Ffaq","\u002Flogin","\u002Fnotice","\u002Fmembers","\u002Fposting"]})
--- admintempu/_nuxt/static/1614613312/notice/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/notice", {data:[{}],fetch:{},mutations:void 0});
