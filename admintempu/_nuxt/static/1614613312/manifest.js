d678c2d9d78464331e75da0ee2ed91b7521e7af0
devilmarte <79588260+devilmarte@users.noreply.github.com>
2021-03-02 00:44:59 +0900

    add admin site

---
+++ admintempu/_nuxt/static/1614613312/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fboard","\u002Ffaq","\u002Flogin","\u002Fnotice","\u002Fmembers","\u002Fposting"]})
---
+++ admintempu/_nuxt/static/1614613312/notice/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/notice", {data:[{}],fetch:{},mutations:void 0});
